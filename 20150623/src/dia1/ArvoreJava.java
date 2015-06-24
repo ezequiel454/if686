1c436c6a4e2962aa02fbca4c10c9a9ef0371c826
ezequiel <ezequiel454@gmail.com>
2015-06-23 22:40:28 -0300

    final
    ok

--- 20150623/src/dia1/ArvoreJava.java
+++ 20150623/src/dia1/ArvoreJava.java
@@ -13,14 +13,58 @@ public class ArvoreJava {
 		Thread[] t = new Thread[50];
 		Tree <Integer> tree = new Tree <Integer> ();
 		
-		for (int i = 0; i<50 ; i++ ) {
+		for (int i = 0; i<2 ; i++ ) {
 			t[i] = new InsereT (tree);
 			System.out.println(100000000);
 			t[i].start();
+		}
+		
+		for(int i = 0; i < 2; i++){
+			try {
+				t[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		
 	}
 }
 
+
+class InsereT extends Thread {
+	Tree <Integer> tree = new Tree <Integer> ();
+	int value;
+	Random randomNumber = new Random();
+
+	/*
+	public InsereT (){
+		for (int i = 0; i < 5 ; i++) {
+			value = randomNumber.nextInt(1000);
+			//System.out.println(value);
+			tree.insertNode(value);
+			//tree.inorderTraversal();
+		}
+		
+	}
+*/
+	public InsereT (Tree <Integer> tre){
+		tree = tre;
+	}
+	
+	
+	public void run (){
+		
+		for (int i = 0; i < 5 ; i++) {
+			
+			value = randomNumber.nextInt(1000);
+			System.out.println(value + "/n");
+			System.out.println("ok ne so me da 1 plx ");
+			tree.insertNode(value);
+			System.out.println("espaco e bom");
+			tree.inorderTraversal();
+		}
+
+	}
+}
 
